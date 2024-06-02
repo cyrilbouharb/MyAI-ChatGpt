85f56136c269764d53c42d12cdf750aaeb4253c5
Cyril Bou-Harb <cyrilobouharb@outlook.com>
2024-06-02 10:59:27 +0400

    initial setup of node, express and integration og mongodb connection and api keys

---
+++ backend/src/index.ts
@@ -0,0 +1,9 @@
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
+
+const PORT = process.env.PORT || 5000;
+//connectiond and listeneres
+connectToDatabase().then(() => {
+  app.listen(PORT, () => console.log("Server Open & connected to Database"));
+})
+.catch((err) => console.log(err));
