92c1912ec420a2e0d01252aecde8da7ef4bdb829
Cyril Bou-Harb <cyrilobouharb@outlook.com>
2024-06-04 00:56:18 +0400

    signup + login + authentication & cookies

---
+++ backend/dist/controllers/user-controllers.js
@@ -0,0 +1,69 @@
+import User from "../models/User.js";
+import { hash, compare } from 'bcrypt';
+import { createToken } from "../utils/token-manager.js";
+import { COOKIE_NAME } from "../utils/constants.js";
+export const getAllUsers = async (req, res, next) => {
+    //get all users from db
+    try {
+        const users = await User.find();
+        return res.status(200).json({ message: "OK", users });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
+};
+export const userSignup = async (req, res, next) => {
+    // user signup 
+    try {
+        const { name, email, password } = req.body;
+        const exisitingUser = await User.findOne({ email });
+        if (exisitingUser)
+            return res.status(401).send("User aleady registered");
+        const hashedPassword = await hash(password, 10);
+        const user = new User({ name, email, password: hashedPassword });
+        await user.save();
+        //create token and store cookie
+        const token = createToken(user._id.toString(), user.email, "7d");
+        const expires = new Date();
+        expires.setDate(expires.getDate() + 7);
+        res.cookie(COOKIE_NAME, token, { path: "/", domain: "localhost", expires, httpOnly: true, signed: true, });
+        return res.status(201).json({ message: "OK", id: user._id.toString() });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
+};
+export const userLogin = async (req, res, next) => {
+    // user login
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).send("User not registered");
+        }
+        const isPasswordCorrect = await compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(403).send("Incorrect Password");
+        }
+        //clear previous cookie
+        res.clearCookie(COOKIE_NAME, {
+            httpOnly: true,
+            domain: "localhost",
+            signed: true,
+            path: "/",
+        });
+        // create a new token and store cookie
+        const token = createToken(user._id.toString(), user.email, "7d");
+        const expires = new Date();
+        expires.setDate(expires.getDate() + 7);
+        res.cookie(COOKIE_NAME, token, { path: "/", domain: "localhost", expires, httpOnly: true, signed: true, });
+        return res.status(200).json({ message: "OK", id: user._id.toString() });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
+};
+//# sourceMappingURL=user-controllers.js.map
