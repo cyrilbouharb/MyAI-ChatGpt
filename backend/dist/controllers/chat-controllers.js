24889d35d8f0c1a9c10119af79f3f7231f10f621
Cyril Bou-Harb <cyrilobouharb@outlook.com>
2024-06-23 21:03:46 +0400

    delete chats + user logout

--- backend/dist/controllers/chat-controllers.js
+++ backend/dist/controllers/chat-controllers.js
@@ -50,4 +50,24 @@ export const sendChatsToUser = async (req, res, next) => {
         return res.status(200).json({ message: "ERROR", cause: error.message });
     }
 };
+export const deleteChats = async (req, res, next) => {
+    try {
+        //checking user token
+        const user = await User.findById(res.locals.jwtData.id);
+        if (!user) {
+            return res.status(401).send("User not registered OR Token malfunctioned");
+        }
+        if (user._id.toString() !== res.locals.jwtData.id) {
+            return res.status(401).send("Invalid Permissions");
+        }
+        //@ts-ignore
+        user.chats = [];
+        await user.save();
+        return res.status(200).json({ message: "OK" });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
+};
 //# sourceMappingURL=chat-controllers.js.map
--- backend/src/controllers/chat-controllers.ts
+++ backend/src/controllers/chat-controllers.ts
@@ -60,3 +60,24 @@ export const sendChatsToUser = async (req: Request, res: Response, next: NextFun
 
     }
 }
+
+export const deleteChats = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        //checking user token
+        const user = await User.findById(res.locals.jwtData.id);
+        if (!user) {
+            return res.status(401).send("User not registered OR Token malfunctioned");
+        }
+        if (user._id.toString() !== res.locals.jwtData.id) {
+            return res.status(401).send("Invalid Permissions");
+        }
+        //@ts-ignore
+        user.chats = [];
+        await user.save();
+        return res.status(200).json({message: "OK"});
+    } catch (error) {
+        console.log(error);
+        return res.status(200).json({message: "ERROR", cause: error.message});
+
+    }
+}
